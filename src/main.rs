ebc31b192362d2d7c9d6cec478897e9413239602
Henry Nguyen <henmmi@outlook.com>
2024-02-02 17:40:00 +1030

    hello world

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod app;
+
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <h1>{ "Hello World" }</h1>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
