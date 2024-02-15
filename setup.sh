c5e16bea4216ce8b6c9c75628074d254d01c1840
Henry Nguyen <henmmi@outlook.com>
2024-02-15 17:35:32 +1030

    add build command

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-PATH=$PATH:/vercel/.cargo/bin
-
-cargo install --locked trunk
-
-rustup target add wasm32-unknown-unknown
-
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
 mv tailwindcss-linux-x64 tailwindcss
 chmod +x tailwindcss
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+PATH=$PATH:/vercel/.cargo/bin
+
+
+rustup target add wasm32-unknown-unknown
+cargo install wasm-bindgen-cli
+cargo install --locked trunk
+
