8eaf4a7dc2eb1317b5ffed5df02936146db81be6
Henry Nguyen <henry@framepath.org>
2026-05-31 14:22:21 +0930

    fix: update build and deploy

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
-mv tailwindcss-linux-x64 tailwindcss
-chmod +x tailwindcss
-
 curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 
